c86aba5f5578d5e5186ebeb49f104dea2848690c
topherwhite <topherwhite@gmail.com>
2014-12-29 23:29:47 +0100

    decent overhaul of HttpGet along with updater app

--- src/org/rfcx/guardian/utility/HttpGet.java
+++ src/org/rfcx/guardian/utility/HttpGet.java
@@ -29,28 +29,36 @@ public class HttpGet {
 	private static final String TAG = HttpGet.class.getSimpleName();
 	private static final String NULL_EXC = "Exception thrown, but exception itself is null.";
 	private static final String DOWNLOAD_TIME_LABEL = "Download time: ";
-
-    // need to make these longer...
-	private static int requestReadTimeout = 10000;
+	
+	private boolean verboseLogging = false;
+	
+    // need to make these longer and/or dynamic
+	// (dynamic is better but methods that reference them can't be static...)
+	private static int requestReadTimeout = 600000;
 	private static int requestConnectTimeout = 30000;
 	private static boolean useCaches = false;
 	
 	
-	public JSONObject getAsJson(String fullUrl, List<String[]> keyValueParameters) throws ParseException {
+	public JSONObject getAsJson(String fullUrl, List<String[]> keyValueParameters) {
 		long startTime = Calendar.getInstance().getTimeInMillis();
 		String str = doGetString(fullUrl,keyValueParameters);
-		Log.d(TAG,DOWNLOAD_TIME_LABEL+(Calendar.getInstance().getTimeInMillis()-startTime)+"ms");
-		return (JSONObject) (new JSONParser()).parse(str);
+		if (verboseLogging) { Log.d(TAG,DOWNLOAD_TIME_LABEL+(Calendar.getInstance().getTimeInMillis()-startTime)+"ms"); }
+		try {
+			return (JSONObject) (new JSONParser()).parse(str);
+		} catch (ParseException e) {
+			Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
+		}
+		return null;
 	}
 	
-	public JSONObject getAsJson(String fullUrl) throws ParseException {
+	public JSONObject getAsJson(String fullUrl) {
 		return getAsJson(fullUrl, (new ArrayList<String[]>()));
 	}
 	
 	public String getAsString(String fullUrl, List<String[]> keyValueParameters) {
 		long startTime = Calendar.getInstance().getTimeInMillis();
 		String str = doGetString(fullUrl,keyValueParameters);
-		Log.d(TAG,DOWNLOAD_TIME_LABEL+(Calendar.getInstance().getTimeInMillis()-startTime)+"ms");
+		if (verboseLogging) { Log.d(TAG,DOWNLOAD_TIME_LABEL+(Calendar.getInstance().getTimeInMillis()-startTime)+"ms"); }
 		return str;
 	}
 	
@@ -58,29 +66,36 @@ public class HttpGet {
 		return getAsString(fullUrl,(new ArrayList<String[]>()));
 	}
 	
-	public boolean getAsFile(String fullUrl, List<String[]> keyValueParameters, String outputFileName, Context context) throws MalformedURLException, FileNotFoundException, IOException {
+	public boolean getAsFile(String fullUrl, List<String[]> keyValueParameters, String outputFileName, Context context) {
 		long startTime = Calendar.getInstance().getTimeInMillis();
-		StringBuilder url = (new StringBuilder()).append(fullUrl).append("?");
+		StringBuilder url = (new StringBuilder()).append(fullUrl);
+		if (keyValueParameters.size() > 0) url.append("?");
 		for (String[] keyValue : keyValueParameters) {
 			url.append(keyValue[0]).append("=").append(keyValue[1]).append("&");
 		}
-		writeFileResponseStream(
-				httpGetFileInputStream(url.toString()),
-				httpGetFileOutputStream(outputFileName,context)
-			);
-		Log.d(TAG,DOWNLOAD_TIME_LABEL+(Calendar.getInstance().getTimeInMillis()-startTime)+"ms");
-		return (new File(context.getFilesDir(), outputFileName)).exists();
+		if (verboseLogging) { Log.d(TAG,"HTTP GET: "+url.toString()); }
+		FileOutputStream fileOutputStream = httpGetFileOutputStream(outputFileName,context);
+		InputStream inputStream = httpGetFileInputStream(url.toString());
+		if ((inputStream != null) && (fileOutputStream != null)) {
+			writeFileResponseStream(inputStream,fileOutputStream);
+			closeInputOutputStreams(inputStream,fileOutputStream);
+			if (verboseLogging) { Log.d(TAG,DOWNLOAD_TIME_LABEL+(Calendar.getInstance().getTimeInMillis()-startTime)+"ms"); }
+			return (new File(context.getFilesDir(), outputFileName)).exists();
+		}
+		return false;
 	}
 	
-	public boolean getAsFile(String fullUrl, String outputFileName, Context context) throws MalformedURLException, FileNotFoundException, IOException {
+	public boolean getAsFile(String fullUrl, String outputFileName, Context context) {
 		return getAsFile(fullUrl, (new ArrayList<String[]>()), outputFileName, context);
 	}	
 	
 	private static String doGetString(String fullUrl, List<String[]> keyValueParameters) {
-		StringBuilder url = (new StringBuilder()).append(fullUrl).append("?");
+		StringBuilder url = (new StringBuilder()).append(fullUrl);
+		if (keyValueParameters.size() > 0) url.append("?");
 		for (String[] keyValue : keyValueParameters) {
 			url.append(keyValue[0]).append("=").append(keyValue[1]).append("&");
 		}
+		Log.d(TAG,"HTTP GET: "+url.toString());
 		return executeGet(url.toString());
 	}
     
@@ -92,12 +107,12 @@ public class HttpGet {
 			} else if (inferredProtocol.equals("https")) {
 				return sendSecureGetRequest((new URL(fullUrl)));
 			} else {
-				return "Inferred protocol was neither HTTP nor HTTPS.";
+				Log.e(TAG, "Inferred protocol was neither HTTP nor HTTPS.");
 			}
 		} catch (MalformedURLException e) {
 			Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
-			return "Bad URL";
 		}
+		return null;
 	}
 	
 	private static String sendInsecureGetRequest(URL url) {
@@ -113,11 +128,13 @@ public class HttpGet {
 	        conn.connect();
 		    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
 	            return readResponseStream(conn.getInputStream());
+	        } else {
+	        	Log.e(TAG, "HTTP Code: "+conn.getResponseCode());
 	        }
 	    } catch (Exception e) {
 	    	Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
 	    }
-	    return "The request returned an error.";        
+	    return null;        
 	}
 	
 	private static String sendSecureGetRequest(URL url) {
@@ -133,11 +150,13 @@ public class HttpGet {
 	        conn.connect();
 		    if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
 	            return readResponseStream(conn.getInputStream());
+	        } else {
+	        	Log.e(TAG, "HTTP Code: "+conn.getResponseCode());
 	        }
 	    } catch (Exception e) {
 	    	Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
 	    }
-	    return "The request returned an error.";    
+	    return null;    
 	}
 
 	private static String readResponseStream(InputStream inputStream) {
@@ -163,56 +182,85 @@ public class HttpGet {
 	    return stringBuilder.toString();
 	} 
 
-	private static void writeFileResponseStream(InputStream inputStream, FileOutputStream fileOutputStream) throws IOException {
-		byte[] buffer = new byte[8192];
-		int bufferLength = 0;
-		while ((bufferLength = inputStream.read(buffer)) != -1) {
-			fileOutputStream.write(buffer, 0, bufferLength);
+	private static void writeFileResponseStream(InputStream inputStream, FileOutputStream fileOutputStream) {
+		try {
+			byte[] buffer = new byte[8192];
+			int bufferLength = 0;
+			while ((bufferLength = inputStream.read(buffer)) != -1) {
+				fileOutputStream.write(buffer, 0, bufferLength);
+			}
+		} catch (IOException e) {
+			Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
 		}
-		inputStream.close();
-		fileOutputStream.flush();
-		fileOutputStream.close();
 	}
 	
-	private static FileOutputStream httpGetFileOutputStream(String fileName, Context context) throws FileNotFoundException {
-		String filesDir = context.getFilesDir().toString();
-		if ((new File(filesDir,fileName)).exists()) { (new File(filesDir,fileName)).delete(); }
-		return context.openFileOutput(fileName, Context.MODE_WORLD_READABLE|Context.MODE_WORLD_WRITEABLE);
+	private static void closeInputOutputStreams(InputStream inputStream, FileOutputStream fileOutputStream) {
+		try {
+			inputStream.close();
+			fileOutputStream.flush();
+			fileOutputStream.close();
+		} catch (IOException e) {
+			Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
+		}
 	}
 	
-	private static InputStream httpGetFileInputStream(String fullUrl) throws MalformedURLException, IOException {
-    	String inferredProtocol = fullUrl.substring(0, fullUrl.indexOf(":"));
-		if (inferredProtocol.equals("http")) {
-			HttpsURLConnection conn = (HttpsURLConnection) (new URL(fullUrl)).openConnection();
-	        conn.setReadTimeout(requestReadTimeout);
-	        conn.setConnectTimeout(requestConnectTimeout);
-	        conn.setRequestMethod("GET");
-	        conn.setUseCaches(useCaches);
-	        conn.setDoInput(true);
-	        conn.setDoOutput(true);
-	        conn.setRequestProperty("Connection", "Keep-Alive");
-	        conn.connect();
-	        if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
-	        	return conn.getInputStream();
-	        }
-		} else if (inferredProtocol.equals("https")) {
-			HttpURLConnection conn = (HttpURLConnection) (new URL(fullUrl)).openConnection();
-	        conn.setReadTimeout(requestReadTimeout);
-	        conn.setConnectTimeout(requestConnectTimeout);
-	        conn.setRequestMethod("GET");
-	        conn.setUseCaches(useCaches);
-	        conn.setDoInput(true);
-	        conn.setDoOutput(true);
-	        conn.setRequestProperty("Connection", "Keep-Alive");
-	        conn.connect();
-	        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-	        	return conn.getInputStream();
-	        }
-		} else {
-			Log.e(TAG,"Inferred protocol was neither HTTP nor HTTPS.");
+	private static FileOutputStream httpGetFileOutputStream(String fileName, Context context) {
+		File targetFile = new File(context.getFilesDir().toString()+"/"+fileName);
+		if (targetFile.exists()) { targetFile.delete(); }
+		try {
+			return context.openFileOutput(fileName, Context.MODE_WORLD_READABLE|Context.MODE_WORLD_WRITEABLE);
+		} catch (FileNotFoundException e) {
+			Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
 		}
 		return null;
 	}
+	
+	private static InputStream httpGetFileInputStream(String fullUrl) {
+    	String inferredProtocol = fullUrl.substring(0, fullUrl.indexOf(":"));
+    	try {
+			if (inferredProtocol.equals("http")) {
+				HttpsURLConnection conn = (HttpsURLConnection) (new URL(fullUrl)).openConnection();
+		        conn.setReadTimeout(requestReadTimeout);
+		        conn.setConnectTimeout(requestConnectTimeout);
+		        conn.setRequestMethod("GET");
+		        conn.setUseCaches(useCaches);
+		        conn.setDoInput(true);
+		        conn.setDoOutput(true);
+		        conn.setRequestProperty("Connection", "Keep-Alive");
+		        conn.connect();
+		        if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+		        	return conn.getInputStream();
+		        } else {
+		        	Log.e(TAG, "HTTP Code: "+conn.getResponseCode());
+		        }
+			} else if (inferredProtocol.equals("https")) {
+				HttpURLConnection conn = (HttpURLConnection) (new URL(fullUrl)).openConnection();
+		        conn.setReadTimeout(requestReadTimeout);
+		        conn.setConnectTimeout(requestConnectTimeout);
+		        conn.setRequestMethod("GET");
+		        conn.setUseCaches(useCaches);
+		        conn.setDoInput(true);
+		        conn.setDoOutput(true);
+		        conn.setRequestProperty("Connection", "Keep-Alive");
+		        conn.connect();
+		        if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+		        	return conn.getInputStream();
+		        } else {
+		        	Log.e(TAG, "HTTP Code: "+conn.getResponseCode());
+		        }
+			} else {
+				Log.e(TAG,"Inferred protocol was neither HTTP nor HTTPS.");
+			}
+    	} catch (MalformedURLException e) {
+    		Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
+    	} catch (IOException e) {
+    		Log.e(TAG,(e!=null) ? (e.getMessage() + TextUtils.join(" | ", e.getStackTrace())) : NULL_EXC);
+    	}
+		return null;
+	}
 
-
+	public void useVerboseLogging(boolean yesNo) {
+		this.verboseLogging = yesNo;
+	}
+	
 }
