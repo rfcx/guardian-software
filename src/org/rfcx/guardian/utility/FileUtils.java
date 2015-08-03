77cdc412c556de8b664103e17db1ebd576c13df8
Topher White <t@pher.me>
2015-08-03 03:07:08 +0200

    recursive delete method

--- src/org/rfcx/guardian/utility/FileUtils.java
+++ src/org/rfcx/guardian/utility/FileUtils.java
@@ -88,4 +88,19 @@ public class FileUtils {
 	    outputStream.close();
 	}
 	
+	
+	public static boolean delete(String filePath, boolean recursive) {
+		File file = new File(filePath);
+		
+		if (!file.exists()) { return true; }
+		if (!recursive || !file.isDirectory()) { return file.delete(); }
+
+		String[] list = file.list();
+		for (int i = 0; i < list.length; i++) {
+			if (!delete(filePath + File.separator + list[i], true))
+				return false;
+		}
+		return file.delete();
+	}
+	
 }
