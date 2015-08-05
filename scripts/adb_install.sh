b96606f495b81afaa38b20ae92952f5b8600f2d5
Topher White <t@pher.me>
2015-08-05 14:43:41 +0200

    start of build and maintenance scripts

---
+++ scripts/adb_install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $SCRIPT_DIR/../;
+
+if [ ! -d tmp ]; then
+  echo "creating tmp directories";
+  mkdir scripts/tmp;
+fi
+
+#cd ~/Downloads/;
+#adb push 0.4.72.apk /data/data/org.rfcx.guardian.updater/files/0.4.72.apk; adb shell pm install -r /data/data/org.rfcx.guardian.updater/files/0.4.72.apk; adb shell rm /data/data/org.rfcx.guardian.updater/files/0.4.72.apk;
