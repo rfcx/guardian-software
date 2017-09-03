adf1b3b28d9c9404784b2e4c05fb32fcc32ae939
Topher <topherwhite@gmail.com>
2017-09-03 23:35:37 +0000

    removing mp3 encoding from library

--- rfcx-guardian-lib-audio/jni/Application.mk
+++ rfcx-guardian-lib-audio/jni/Application.mk
@@ -1,5 +1,7 @@
 
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi # armeabi-v7a # armeabi x86 arm64-v8a
+APP_ABI := armeabi
+#APP_ABI := armeabi-v7a
+#APP_API := arm64-v8a
 
-APP_PLATFORM := android-8
+APP_PLATFORM := android-14
