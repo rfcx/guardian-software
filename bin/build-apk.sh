ec5ebd7f169d8bc4a0e582a795786962b1efc7e1
Topher <topherwhite@gmail.com>
2018-07-24 01:57:34 -0700

    building libraries automatically as well

--- bin/build-apk.sh
+++ bin/build-apk.sh
@@ -25,8 +25,12 @@ else
   echo ""; echo "RFCx $ROLE ($APK_VERSION)";
   
   export SDK_VERSION=`cat $SCRIPT_DIR/../rfcx-guardian-role-$ROLE/AndroidManifest.xml | grep 'android:targetSdkVersion=' | cut -d'"' -f 2`;
-  
-  echo "generating build configuration... (android-$SDK_VERSION)";
+
+  echo "generating build configurations... (android-$SDK_VERSION)";
+  export BUILD_CONFIG_LIB_UTIL=`$ANDROID_HOME/tools/android update project --path ../rfcx-guardian-lib-utility --name rfcx-guardian-lib-utility --target android-$SDK_VERSION --subprojects`;
+  export BUILD_CONFIG_LIB_AUDIO=`$ANDROID_HOME/tools/android update project --path ../rfcx-guardian-lib-audio --name rfcx-guardian-lib-audio --target android-$SDK_VERSION --subprojects`;
+  export BUILD_CONFIG_LIB_I2C=`$ANDROID_HOME/tools/android update project --path ../rfcx-guardian-lib-i2c --name rfcx-guardian-lib-i2c --target android-$SDK_VERSION --subprojects`;
+
   export BUILD_CONFIG=`$ANDROID_HOME/tools/android update project --path . --name rfcx-guardian-role-$ROLE --target android-$SDK_VERSION --subprojects`;
 
   echo "key.store=$SCRIPT_DIR/_private/rfcx-guardian-keystore.jks" >> local.properties;
@@ -67,7 +71,8 @@ else
   echo "copying apk into tmp directory..."
   cp $APK_PATH ../tmp/$ROLE-$APK_VERSION.apk;
 
-  export POST_CLEANUP=`cd $SCRIPT_DIR/../rfcx-guardian-role-$ROLE; rm local.properties build.xml proguard-project.txt;`;
+  export POST_CLEANUP_ROLES=`cd $SCRIPT_DIR/../; rm rfcx-guardian-role-*/local.properties rfcx-guardian-role-*/build.xml rfcx-guardian-role-*/proguard-project.txt;`;
+  export POST_CLEANUP_LIBS=`cd $SCRIPT_DIR/../; rm rfcx-guardian-lib-*/local.properties rfcx-guardian-lib-*/build.xml rfcx-guardian-lib-*/proguard-project.txt;`;
 
   cd $SCRIPT_DIR/../;
 
