f0919b5acf6c3824eadb7de2b70591c6e960bf2e
Antony Harfield <antonyharfield@gmail.com>
2019-03-26 14:43:23 +0700

    Add ndk config

--- audio/jni/Application.mk
+++ audio/jni/Application.mk
@@ -1,6 +1,4 @@
-
-APP_ABI := armeabi
-#APP_ABI := armeabi-v7a 		# armv7 is significantly faster due to hardware fpu, but it cannot run on older devices
-#APP_API := arm64-v8a
-
-APP_PLATFORM := android-10
+#APP_ABI := all
+APP_ABI := armeabi-v7a arm64-v8a
+APP_PLATFORM := android-16
+APP_ALLOW_MISSING_DEPS := true
