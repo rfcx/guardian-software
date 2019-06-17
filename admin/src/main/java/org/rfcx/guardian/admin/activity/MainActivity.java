80b9b0288556f081cadfa9cdc032f177e90f95ce
Tooseriuz <wasuphonn59@email.nu.ac.th>
2019-06-18 05:37:44 +0700

    add actiobar for admin role

--- admin/src/main/java/org/rfcx/guardian/admin/activity/MainActivity.java
+++ admin/src/main/java/org/rfcx/guardian/admin/activity/MainActivity.java
@@ -1,5 +1,10 @@
 package org.rfcx.guardian.admin.activity;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import org.rfcx.guardian.admin.R;
 
 import org.rfcx.guardian.admin.RfcxGuardian;
@@ -8,7 +13,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 	
 
 	@Override
@@ -54,6 +59,10 @@ public class MainActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
 	}
 
 	@Override
