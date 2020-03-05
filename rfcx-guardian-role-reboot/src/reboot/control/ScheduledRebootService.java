d12e3af8ad37cd7c89a5c411ad547b6a8533b678
Topher <topherwhite@gmail.com>
2020-03-05 20:49:03 +0800

    reboot role with safer reboot mode

---
+++ rfcx-guardian-role-reboot/src/reboot/control/ScheduledRebootService.java
@@ -0,0 +1,31 @@
+package reboot.control;
+
+import android.app.IntentService;
+import android.content.Intent;
+import reboot.RfcxGuardian;
+import rfcx.utility.rfcx.RfcxLog;
+import rfcx.utility.service.RfcxServiceHandler;
+
+public class ScheduledRebootService extends IntentService {
+	
+	private static final String logTag = RfcxLog.generateLogTag(RfcxGuardian.APP_ROLE, ScheduledRebootService.class);
+	
+	private static final String SERVICE_NAME = "ScheduledReboot";
+		
+	public ScheduledRebootService() {
+		super(logTag);
+	}
+	
+	@Override
+	protected void onHandleIntent(Intent inputIntent) {
+		Intent intent = new Intent(RfcxServiceHandler.intentServiceTags(false, RfcxGuardian.APP_ROLE, SERVICE_NAME));
+		sendBroadcast(intent, RfcxServiceHandler.intentServiceTags(true, RfcxGuardian.APP_ROLE, SERVICE_NAME));;
+		
+		RfcxGuardian app = (RfcxGuardian) getApplication();
+		
+		app.rfcxServiceHandler.triggerService("RebootTrigger", true);
+	
+	}
+	
+	
+}
