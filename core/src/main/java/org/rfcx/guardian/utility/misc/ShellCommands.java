28e8726614a7172a24c1c67c7ff64fd18c3dd2d3
Topher <topherwhite@gmail.com>
2020-03-24 11:51:44 -0700

    added a dedicated utility class for reboot methods.

--- core/src/main/java/org/rfcx/guardian/utility/misc/ShellCommands.java
+++ core/src/main/java/org/rfcx/guardian/utility/misc/ShellCommands.java
@@ -168,25 +168,25 @@ public class ShellCommands {
 		executeCommandInShell("pm list features", true, context);
 	}
 	
-	public static boolean triggerRebootAsRoot(Context context) {
-		
-		RfcxGarbageCollection.runAndroidGarbageCollection();
-		
-		int rebootPreDelay = 2;
-
-		Log.v(logTag, "Attempting graceful reboot... then after "+rebootPreDelay+" seconds, killing RFCx processes and forcing reboot...");
-		
-		executeCommandAsRoot(""
-				+"am start -a android.intent.action.REBOOT; "
-				+"am broadcast android.intent.action.ACTION_SHUTDOWN; "
-				+"sleep "+rebootPreDelay
-					+" && kill $(ps | grep org.rfcx.org.rfcx.guardian.guardian | cut -d \" \" -f 5)"
-					+" && umount -vl "+Environment.getExternalStorageDirectory().toString()
-					+" && reboot; "
-				+"sleep "+rebootPreDelay+" && reboot; "
-			, context);
-		return true;
-	}
+//	public static boolean triggerRebootAsRoot(Context context) {
+//
+//		RfcxGarbageCollection.runAndroidGarbageCollection();
+//
+//		int rebootPreDelay = 3;
+//
+//		Log.v(logTag, "Attempting graceful reboot... then after "+rebootPreDelay+" seconds, killing RFCx processes and forcing reboot...");
+//
+//		executeCommandAsRoot(""
+//				+"am start -a android.intent.action.REBOOT; "
+//				+"am broadcast android.intent.action.ACTION_SHUTDOWN; "
+//				+"sleep "+rebootPreDelay
+//					+" && kill $(ps | grep org.rfcx.org.rfcx.guardian.guardian | cut -d \" \" -f 5)"
+//					+" && umount -vl "+Environment.getExternalStorageDirectory().toString()
+//					+" && reboot; "
+//				+"sleep "+rebootPreDelay+" && reboot; "
+//			, context);
+//		return true;
+//	}
 	
 	
 	
