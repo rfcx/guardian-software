9b2a9591503fa9d016be370afba9223ee73706aa
topherwhite <t@pher.me>
2020-04-01 03:16:42 -0700

    updated to use apksigner and remove NDK code

--- bin/build-apk.sh
+++ bin/build-apk.sh
@@ -4,7 +4,11 @@ export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 export ROLE=$1;
 
-# export KEY_PSWD=`cat $SCRIPT_DIR/_private/rfcx-guardian-keystore-pswd.txt;`;
+export APKSIGNER_BIN="$ANDROID_SDK_ROOT/build-tools/28.0.3/apksigner";
+
+export KEYSTORE="$SCRIPT_DIR/_private/rfcx-platform-keystore.jks";
+export KEY_ALIAS=`cat $SCRIPT_DIR/_private/rfcx-platform-keystore-alias.txt;`;
+export KEY_PSWD=`cat $SCRIPT_DIR/_private/rfcx-platform-keystore-pswd.txt;`;
 
 cd $SCRIPT_DIR/../;
 
@@ -22,41 +26,10 @@ export APK_TYPE="release"; #"debug";
 
 echo ""; echo "RFCx $ROLE ($APK_VERSION)";
 
-# export SDK_VERSION=`cat $ROLE_DIR/AndroidManifest.xml | grep 'android:targetSdkVersion=' | cut -d'"' -f 2`;
-
-# echo "generating build configurations... (android-$SDK_VERSION)";
-# export BUILD_CONFIG_LIB_UTIL=`$ANDROID_HOME/tools/android update project --path $ROLE_DIR/../rfcx-guardian-lib-utility --name rfcx-guardian-lib-utility --target android-$SDK_VERSION --subprojects`;
-# export BUILD_CONFIG_LIB_AUDIO=`$ANDROID_HOME/tools/android update project --path $SCRIPT_DIR/../rfcx-guardian-lib-audio --name rfcx-guardian-lib-audio --target android-$SDK_VERSION --subprojects`;
-# export BUILD_CONFIG_LIB_I2C=`$ANDROID_HOME/tools/android update project --path $SCRIPT_DIR/../rfcx-guardian-lib-i2c --name rfcx-guardian-lib-i2c --target android-$SDK_VERSION --subprojects`;
-
-# export BUILD_CONFIG=`$ANDROID_HOME/tools/android update project --path . --name rfcx-guardian-role-$ROLE --target android-$SDK_VERSION --subprojects`;
-
-# echo "key.store=$SCRIPT_DIR/_private/rfcx-guardian-keystore.jks" >> local.properties;
-# echo "key.alias=rfcx-guardian-android" >> local.properties;
-# echo "key.store.password=$KEY_PSWD" >> local.properties;
-# echo "key.alias.password=$KEY_PSWD" >> local.properties;
-
-# # include rfcx shared 'utility' library
-# echo "android.library.reference.1=../rfcx-guardian-lib-utility" >> local.properties;
-
-# for the 'guardian' role, include rfcx shared audio encoding library
-# if [[ $ROLE = 'guardian' ]]; then
-  
-#   echo "android.library.reference.2=../rfcx-guardian-lib-audio" >> local.properties;
-#   cd $ROLE_DIR/../rfcx-guardian-lib-audio/jni && $ANDROID_HOME/ndk-bundle/ndk-build;
-
-# fi
-
-# # for the 'admin' role, include rfcx shared i2c access library
-# if [[ $ROLE = 'admin' ]]; then
-  
-# #   echo "android.library.reference.2=../rfcx-guardian-lib-i2c" >> local.properties;
-#   cd $ROLE_DIR/../rfcx-guardian-lib-i2c/jni && $ANDROID_HOME/ndk-bundle/ndk-build;
-
-#   cp $ROLE_DIR/../rfcx-guardian-lib-i2c/libs/armeabi/i2cget $ROLE_DIR/assets/i2cget;
-#   cp $ROLE_DIR/../rfcx-guardian-lib-i2c/libs/armeabi/i2cset $ROLE_DIR/assets/i2cset;
-   
-# fi
+##
+##	NDK native compilation has been left out as we migrate toward building with gradle.
+##  We'll need to rewrite this later, as it becomes needed.
+##
 
 cd $PROJECT_DIR;
 
@@ -68,17 +41,16 @@ elif [ "$APK_TYPE" = "release" ]; then
 	gradlew :$ROLE:assembleRelease;
 fi
 
-# export APK_PATH=`ant release | grep 'Release Package:' | cut -d':' -f 2 | cut -d' ' -f 2`;
 export APK_PATH_UNSIGNED="$ROLE_DIR/build/outputs/apk/$APK_TYPE/$ROLE-$APK_VERSION-$APK_TYPE-unsigned.apk";
+export APK_PATH_SIGNED="$PROJECT_DIR/tmp/$ROLE-$APK_VERSION.apk";
 
 if [ -f "$PROJECT_DIR/tmp/$ROLE-$APK_VERSION.apk" ]; then
 	export DELETE_OLD_APK=`rm $PROJECT_DIR/tmp/$ROLE-$APK_VERSION.apk`;
 fi
 echo "signing apk and placing signed version into tmp directory..."
-java -jar $SCRIPT_DIR/tools/signapk.jar $SCRIPT_DIR/_private/platform.x509.pem $SCRIPT_DIR/_private/platform.pk8 $APK_PATH_UNSIGNED $PROJECT_DIR/tmp/$ROLE-$APK_VERSION.apk;
+$APKSIGNER_BIN sign --ks $KEYSTORE --ks-key-alias $KEY_ALIAS --ks-pass pass:$KEY_PSWD --out $APK_PATH_SIGNED $APK_PATH_UNSIGNED;
 
-# export POST_CLEANUP_ROLES=`rm $ROLE_DIR/local.properties $ROLE_DIR/build.xml $ROLE_DIR/proguard-project.txt;`;
-# export POST_CLEANUP_LIBS=`rm $ROLE_DIR/../rfcx-guardian-lib-*/local.properties $ROLE_DIR/../rfcx-guardian-lib-*/build.xml $ROLE_DIR/../rfcx-guardian-lib-*/proguard-project.txt;`;
+export POST_CLEANUP=`rm $APK_PATH_UNSIGNED;`;
 
 cd $SCRIPT_DIR/../;
 
