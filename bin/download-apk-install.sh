178480f60311383bff958b0e876e1485d845b6c8
Tooseriuz <wasuphonn59@email.nu.ac.th>
2020-04-02 20:06:28 +0700

    Changes by comments

--- bin/download-apk-install.sh
+++ bin/download-apk-install.sh
@@ -1,4 +1,4 @@
-export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 
 version=$1
 apps=("guardian" "admin" "updater")
@@ -26,16 +26,16 @@ fi
 
 for app in "${apps[@]}"; do
     #download apk with version from parameter
-    curl -O https://s3-eu-west-1.amazonaws.com/rfcx-install/rfcx-guardian/guardian-android-guardian/production/$app-$1.apk
+    curl -O https://s3-eu-west-1.amazonaws.com/rfcx-install/rfcx-guardian/guardian-android-guardian/production/$app-$version.apk
 
     echo "transferring apk to device...";
-    $ADB_BIN push $SCRIPT_DIR/guardian-$1.apk data/local/tmp/$app-$1.apk;
+    $ADB_BIN push $SCRIPT_DIR/guardian-$version.apk data/local/tmp/$app-$version.apk;
 
     echo "performing installation...";
-    $ADB_BIN shell pm install -f -r data/local/tmp/$app-$1.apk;
+    $ADB_BIN shell pm install -f -r data/local/tmp/$app-$version.apk;
 
     echo "deleting apk...";
-    $ADB_BIN shell rm data/local/tmp/$app-$1.apk;
+    $ADB_BIN shell rm data/local/tmp/$app-$version.apk;
 
     echo "force relaunch of app role...";
     $ADB_BIN shell am start -n org.rfcx.guardian.$app/.activity.MainActivity;
