7c34f2fffee93da22f2e820e28f487e393cc9590
topherwhite <t@pher.me>
2020-04-13 05:33:01 -0700

    Instruction parsing reporting

--- guardian/src/main/java/org/rfcx/guardian/guardian/instructions/InstructionsUtils.java
+++ guardian/src/main/java/org/rfcx/guardian/guardian/instructions/InstructionsUtils.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.rfcx.guardian.guardian.RfcxGuardian;
+import org.rfcx.guardian.utility.datetime.DateTimeUtils;
 import org.rfcx.guardian.utility.rfcx.RfcxLog;
 
 public class InstructionsUtils {
@@ -28,23 +29,20 @@ public class InstructionsUtils {
 	}
 
 	public void processInstructionJson(JSONObject jsonObj) {
-
 		try {
 			if (jsonObj.has("instructions")) {
 				JSONArray instrArr = jsonObj.getJSONArray("instructions");
 				for (int i = 0; i < instrArr.length(); i++) {
 					JSONObject instrObj = instrArr.getJSONObject(i);
 					if (instrObj.has("id")) {
-						Log.d(logTag, "Instruction: "+instrObj.toString());
-
 						String instrId = instrObj.getString("id");
 						String instrType = instrObj.getString("type");
 						String instrCommand = instrObj.getString("command");
-						long instrExecuteAt = (long) Long.parseLong(instrObj.getString("execute_at"));
 						JSONObject instrMetaObj = instrObj.getJSONObject("meta");
+						long instrExecuteAt = (instrObj.getString("execute_at").equalsIgnoreCase("0")) ? System.currentTimeMillis() : (long) Long.parseLong(instrObj.getString("execute_at"));
 
-
-
+						Log.i(logTag, "Instruction Received: ID: "+instrId+", "+instrType+", "+instrCommand+", at "+ DateTimeUtils.getDateTime(instrExecuteAt)+", "+instrMetaObj.toString());
+						
 					}
 				}
 			}
@@ -54,7 +52,6 @@ public class InstructionsUtils {
 			RfcxLog.logExc(logTag, e);
 
 		}
-
 	}
 
 }
