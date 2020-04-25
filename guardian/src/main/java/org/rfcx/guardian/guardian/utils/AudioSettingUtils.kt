e127b8a9811210ce41ffa34324fee0fe400db7f2
wasuphon <wasuphonn59@email.nu.ac.th>
2020-04-25 17:10:40 +0700

    Add sample rate 20000

--- guardian/src/main/java/org/rfcx/guardian/guardian/utils/AudioSettingUtils.kt
+++ guardian/src/main/java/org/rfcx/guardian/guardian/utils/AudioSettingUtils.kt
@@ -20,7 +20,7 @@ object AudioSettingUtils {
         "128 kbps"
     )
 
-    private val sampleRateList = listOf(8000, 12000, 16000, 24000, 44100, 48000)
+    private val sampleRateList = listOf(8000, 12000, 16000, 20000, 24000, 44100, 48000)
     private val bitRateList = listOf(4096, 8192, 12288, 16384, 20480, 24576, 32768, 40960, 49152, 65536, 81920, 98304, 114688, 131072)
 
     fun getSampleRateLabel(sampleRate: Int): String {
