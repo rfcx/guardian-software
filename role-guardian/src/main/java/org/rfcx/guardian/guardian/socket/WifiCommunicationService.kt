d83eae8bd3b576ac35fbf9ccae3ebf2cbde8a002
wasuphon <wasuphonn59@email.nu.ac.th>
2020-08-01 19:15:00 +0700

    Add Log show wificommunication service state

--- role-guardian/src/main/java/org/rfcx/guardian/guardian/socket/WifiCommunicationService.kt
+++ role-guardian/src/main/java/org/rfcx/guardian/guardian/socket/WifiCommunicationService.kt
@@ -2,6 +2,7 @@ package org.rfcx.guardian.guardian.socket
 
 import android.app.IntentService
 import android.content.Intent
+import android.util.Log
 import org.rfcx.guardian.guardian.RfcxGuardian
 import org.rfcx.guardian.utility.rfcx.RfcxLog
 import org.rfcx.guardian.utility.service.RfcxServiceHandler
@@ -16,18 +17,24 @@ class WifiCommunicationService : IntentService("WifiCommunication") {
     )
 
     override fun onHandleIntent(p0: Intent?) {
+        Log.d(logTag, "Starting WifiCommunication service")
         val intent =
             Intent(RfcxServiceHandler.intentServiceTags(false, RfcxGuardian.APP_ROLE, SERVICE_NAME))
         sendBroadcast(
             intent, RfcxServiceHandler.intentServiceTags(true, RfcxGuardian.APP_ROLE, SERVICE_NAME))
 
         val app = application as RfcxGuardian
+
+        app.rfcxServiceHandler.reportAsActive(SERVICE_NAME)
+
         val state = app.rfcxPrefs.getPrefAsBoolean("admin_enable_wifi_socket")
         try {
             if (state) {
+                Log.d(logTag, "Start WifiCommunication service")
                 SocketManager.stopServerSocket()
                 SocketManager.startServerSocket(applicationContext)
             } else {
+                Log.d(logTag, "Stop WifiCommunication service")
                 SocketManager.stopServerSocket()
             }
         } catch (e: Exception) {
