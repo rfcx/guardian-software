ec1296b51738428b2c1c8cf455660634f914ecd4
wasuphon <wasuphonn59@email.nu.ac.th>
2020-09-10 18:43:20 +0700

    Re-add top right menu on admin

--- role-admin/src/main/java/org/rfcx/guardian/admin/activity/MainActivity.kt
+++ role-admin/src/main/java/org/rfcx/guardian/admin/activity/MainActivity.kt
@@ -3,38 +3,34 @@ package org.rfcx.guardian.admin.activity
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
+import android.view.Menu
+import android.view.MenuItem
 import org.rfcx.guardian.admin.R
 import org.rfcx.guardian.admin.RfcxGuardian
 import org.rfcx.guardian.admin.sms.ComposeSmsActivity
 
 class MainActivity : Activity() {
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val app = application as RfcxGuardian
+        when (item.itemId) {
+            R.id.menu_sms_activity -> startActivity(Intent(this, ComposeSmsActivity::class.java))
+            R.id.menu_screenshot -> app.rfcxServiceHandler.triggerService("ScreenShotCapture", true)
+            R.id.menu_sntp -> app.rfcxServiceHandler.triggerService("SntpSyncJob", true)
+            R.id.menu_logcat -> app.rfcxServiceHandler.triggerService("LogCatCapture", true)
+            R.id.menu_reboot -> app.rfcxServiceHandler.triggerService("RebootTrigger", true)
+        }
+        return true
+    }
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        val app = application as RfcxGuardian
-
-        smsActivityButton.setOnClickListener {
-            startActivity(Intent(this, ComposeSmsActivity::class.java))
-        }
-
-        screenshotButton.setOnClickListener {
-            app.rfcxServiceHandler.triggerService("ScreenShotCapture", true)
-        }
-
-        sntpButton.setOnClickListener {
-            app.rfcxServiceHandler.triggerService("SntpSyncJob", true)
-        }
-
-        logcatButton.setOnClickListener {
-            app.rfcxServiceHandler.triggerService("LogCatCapture", true)
-        }
-
-        rebootButton.setOnClickListener {
-            app.rfcxServiceHandler.triggerService("RebootTrigger", true)
-        }
     }
 
     public override fun onResume() {
