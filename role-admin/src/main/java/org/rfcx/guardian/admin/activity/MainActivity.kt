93b8f6cfdbba1e3f7be4af6c9f92f880a698df2b
topherwhite <t@pher.me>
2020-09-23 03:59:37 -0700

    restored separate activity for sms composition

--- role-admin/src/main/java/org/rfcx/guardian/admin/activity/MainActivity.kt
+++ role-admin/src/main/java/org/rfcx/guardian/admin/activity/MainActivity.kt
@@ -9,12 +9,14 @@ import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import kotlinx.android.synthetic.main.activity_compose_sms.*
 import kotlinx.android.synthetic.main.activity_home.*
 import org.rfcx.guardian.admin.R
 import org.rfcx.guardian.admin.RfcxGuardian
+import org.rfcx.guardian.admin.sms.ComposeSmsActivity
 import org.rfcx.guardian.admin.sms.SmsUtils
 
-class MainActivity : Activity(), TextWatcher {
+class MainActivity : Activity() {
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -33,18 +35,8 @@ class MainActivity : Activity(), TextWatcher {
 
         val app = application as RfcxGuardian
 
-        smsMsgEditText.addTextChangedListener(this)
-        smsNumberEditText.addTextChangedListener(this)
-
-        smsNumberEditText.setText("${app.rfcxPrefs.getPrefAsString("api_sms_address")}")
-
-        sendMsgButton.setOnClickListener {
-            //message for sending
-            val msg = smsMsgEditText.text.toString()
-            val number = smsNumberEditText.text.toString()
-            SmsUtils.addImmediateSmsToQueue(number, msg, app.applicationContext)
-            smsMsgEditText.text = null
-            smsNumberEditText.text = null
+        openSmsActivityButton.setOnClickListener {
+            startActivity(Intent(this, ComposeSmsActivity::class.java))
         }
 
         sntpSyncButton.setOnClickListener {
@@ -65,34 +57,6 @@ class MainActivity : Activity(), TextWatcher {
         super.onResume()
         (application as RfcxGuardian).appResume()
 
-        if (Telephony.Sms.getDefaultSmsPackage(this) != packageName) {
-            // App is not default.
-            // Show the "not currently set as the default SMS app" interface
-            notDefaultAppText.visibility = View.VISIBLE
-
-            // Set up a button that allows the user to change the default SMS app
-            changeDefaultAppButton.setOnClickListener {
-                val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
-                intent.putExtra(
-                    Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
-                    packageName
-                )
-                startActivity(intent)
-            }
-        } else {
-            // App is the default.
-            // Hide the "not currently set as the default SMS app" interface
-            changeDefaultAppButton.visibility = View.GONE
-            notDefaultAppText.visibility = View.GONE
-
-            smsMsgEditText.visibility = View.VISIBLE
-            smsNumberEditText.visibility = View.VISIBLE
-            sendMsgButton.visibility = View.VISIBLE
-
-            sntpSyncButton.visibility = View.VISIBLE
-            screenshotButton.visibility = View.VISIBLE
-            rebootButton.visibility = View.VISIBLE
-        }
     }
 
     public override fun onPause() {
@@ -104,14 +68,6 @@ class MainActivity : Activity(), TextWatcher {
         super.onDestroy()
     }
 
-    override fun afterTextChanged(p0: Editable?) {
-    }
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-    }
-
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        sendMsgButton.isEnabled = !p0.isNullOrBlank()
-    }
 
 }
---
+++ role-admin/src/main/java/org/rfcx/guardian/admin/sms/ComposeSmsActivity.kt
@@ -0,0 +1,79 @@
+package org.rfcx.guardian.admin.sms
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Telephony
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_compose_sms.*
+import org.rfcx.guardian.admin.R
+import org.rfcx.guardian.admin.RfcxGuardian
+
+class ComposeSmsActivity : Activity(), TextWatcher {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose_sms)
+
+        val app = application as RfcxGuardian
+
+        smsMsgEditText.addTextChangedListener(this)
+        smsNumberEditText.addTextChangedListener(this)
+
+        smsNumberEditText.setText("${app.rfcxPrefs.getPrefAsString("api_sms_address")}")
+
+        sendMsgButton.setOnClickListener {
+            //message for sending
+            val msg = smsMsgEditText.text.toString()
+            val number = smsNumberEditText.text.toString()
+            SmsUtils.addImmediateSmsToQueue(number, msg, app.applicationContext)
+            smsMsgEditText.text = null
+            smsNumberEditText.text = null
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (Telephony.Sms.getDefaultSmsPackage(this) != packageName) {
+            // App is not default.
+            // Show the "not currently set as the default SMS app" interface
+            notDefaultAppText.visibility = View.VISIBLE
+
+            // Set up a button that allows the user to change the default SMS app
+            changeDefaultAppButton.setOnClickListener {
+                val intent = Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT)
+                intent.putExtra(
+                    Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
+                    packageName
+                )
+                startActivity(intent)
+            }
+        } else {
+            // App is the default.
+            // Hide the "not currently set as the default SMS app" interface
+            changeDefaultAppButton.visibility = View.GONE
+            notDefaultAppText.visibility = View.GONE
+
+            smsMsgEditText.visibility = View.VISIBLE
+            smsNumberEditText.visibility = View.VISIBLE
+            sendMsgButton.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        sendMsgButton.isEnabled = !p0.isNullOrBlank()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
+    }
+}
