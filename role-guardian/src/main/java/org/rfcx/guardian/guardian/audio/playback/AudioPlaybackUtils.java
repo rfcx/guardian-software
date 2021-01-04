97f6af8d519b804af30e8b999744abd6ce67ac9f
topherwhite <t@pher.me>
2021-01-04 15:15:46 -0800

    placeholder functionality for audio playback service

--- role-guardian/src/main/java/org/rfcx/guardian/guardian/audio/playback/AudioPlaybackUtils.java
+++ role-guardian/src/main/java/org/rfcx/guardian/guardian/audio/playback/AudioPlaybackUtils.java
@@ -1,22 +1,7 @@
 package org.rfcx.guardian.guardian.audio.playback;
 
-import android.content.Context;
-import android.util.Log;
-
-import org.rfcx.guardian.audio.EncodeStatus;
-import org.rfcx.guardian.audio.flac.FLACStreamEncoder;
-import org.rfcx.guardian.audio.opus.OpusAudioEncoder;
 import org.rfcx.guardian.guardian.RfcxGuardian;
-import org.rfcx.guardian.utility.asset.RfcxAudioFileUtils;
-import org.rfcx.guardian.utility.misc.FileUtils;
 import org.rfcx.guardian.utility.rfcx.RfcxLog;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
 
 public class AudioPlaybackUtils {
 
