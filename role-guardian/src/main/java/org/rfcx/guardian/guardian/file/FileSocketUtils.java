72d05c90f8617c0547d9b0d1f15cdd284bc81405
wasuphon <wasuphonn59@email.nu.ac.th>
2021-08-25 18:24:43 +0700

    Change the way to get file transfering

--- role-guardian/src/main/java/org/rfcx/guardian/guardian/file/FileSocketUtils.java
+++ role-guardian/src/main/java/org/rfcx/guardian/guardian/file/FileSocketUtils.java
@@ -15,6 +15,7 @@ import org.rfcx.guardian.utility.rfcx.RfcxLog;
 import org.rfcx.guardian.utility.rfcx.RfcxPrefs;
 
 import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -66,50 +67,50 @@ public class FileSocketUtils {
         return prefsEnableSocketServer && (isWifiEnabled || isBluetoothEnabled);
     }
 
-    private void processReceivedJson(String jsonStr) {
-        try {
-            JSONObject json = new JSONObject(jsonStr);
-            String type = json.getString("type"); // type of file
-            if (type.equals("apk")) {
-                processAPKTypeMessage(json);
-            }
-        } catch (JSONException e) {
-            RfcxLog.logExc(logTag, e);
-        }
-    }
+//    private void processReceivedJson(String jsonStr) {
+//        try {
+//            JSONObject json = new JSONObject(jsonStr);
+//            String type = json.getString("type"); // type of file
+//            if (type.equals("apk")) {
+//                processAPKTypeMessage(json);
+//            }
+//        } catch (JSONException e) {
+//            RfcxLog.logExc(logTag, e);
+//        }
+//    }
 
-    private void processAPKTypeMessage(JSONObject json) throws JSONException {
-        String role = json.getString("role");
-        String version = json.getString("version");
-        int amount = json.getInt("amount"); // amount of role to get
-        int index = json.getInt("index"); // index of byte part of APK
-        int size = json.getInt("size"); // size of all index of APK
-        String bytes = json.getString("bytes"); // bytes of part of APK
+//    private void processAPKTypeMessage(JSONObject json) throws JSONException {
+//        String role = json.getString("role");
+//        String version = json.getString("version");
+//        int amount = json.getInt("amount"); // amount of role to get
+//        int index = json.getInt("index"); // index of byte part of APK
+//        int size = json.getInt("size"); // size of all index of APK
+//        String bytes = json.getString("bytes"); // bytes of part of APK
+//
+//        JSONObject resultJson = new JSONObject();
+//
+//        if (index <= size) {
+//            notCombinedBytes.add(bytes);
+//        }
+//        if (index == size) {
+//            StringBuilder sb = new StringBuilder();
+//            for (String str : notCombinedBytes) {
+//                sb.append(str);
+//            }
+//            notCombinedBytes.clear();
+//            ByteArrayInputStream apkInBytes = new ByteArrayInputStream(stringToByteArray(sb.toString()));
+//            boolean writeResult = writeStreamToDisk(apkInBytes, role, version);
+//            resultJson.put(role, writeResult);
+//        }
+//
+//        if (resultJson.length() == amount) {
+//            sendDownloadResult(resultJson.toString());
+//        }
+//    }
 
-        JSONObject resultJson = new JSONObject();
-
-        if (index <= size) {
-            notCombinedBytes.add(bytes);
-        }
-        if (index == size) {
-            StringBuilder sb = new StringBuilder();
-            for (String str : notCombinedBytes) {
-                sb.append(str);
-            }
-            notCombinedBytes.clear();
-            ByteArrayInputStream apkInBytes = new ByteArrayInputStream(stringToByteArray(sb.toString()));
-            boolean writeResult = writeStreamToDisk(apkInBytes, role, version);
-            resultJson.put(role, writeResult);
-        }
-
-        if (resultJson.length() == amount) {
-            sendDownloadResult(resultJson.toString());
-        }
-    }
-
-    private byte[] stringToByteArray(String str) {
-        return Base64.decode(str, Base64.URL_SAFE);
-    }
+//    private byte[] stringToByteArray(String str) {
+//        return Base64.decode(str, Base64.URL_SAFE);
+//    }
 
     public void startServer() {
 
@@ -119,12 +120,24 @@ public class FileSocketUtils {
                 socketUtils.serverSetup();
                 while (true) {
                     InputStream socketInput = socketUtils.socketSetup();
-                    if (socketInput != null) {
-                        String jsonStr = socketUtils.streamSetup(socketInput);
-                        if (jsonStr != null) {
-                            processReceivedJson(jsonStr);
+                    StringBuilder fileName = new StringBuilder();
+                    byte[] fileNameByte = new byte[1028];
+                    int count = 0;
+
+                    //read until reach '|'
+                    do {
+                        count++;
+                    } while (socketInput.read(fileNameByte, 0, 1) != '|');
+
+                    //get byte read to file name
+                    for (int i = 0; i < count; i++) {
+                        if (fileNameByte[i] == '|') {
+                            break;
                         }
+                        fileName.append(fileNameByte[i]);
                     }
+
+                    writeStreamToDisk(socketInput, fileName.toString());
                 }
             } catch (IOException e) {
                 if (!e.getMessage().equalsIgnoreCase("Socket closed")) {
@@ -137,13 +150,13 @@ public class FileSocketUtils {
         socketUtils.isServerRunning = true;
     }
 
-    private boolean writeStreamToDisk(InputStream body, String role, String version) {
+    private boolean writeStreamToDisk(InputStream body, String fullFileName) {
         try {
             File dir = new File(Environment.getExternalStorageDirectory().toString()+"/rfcx/apk", "softwares");
             if (!dir.exists()) {
                 dir.mkdir();
             }
-            File file = new File(dir, role + "-" + version + "-release.apk.gz");
+            File file = new File(dir, fullFileName);
             InputStream inputStream = null;
             OutputStream outputStream = null;
 
