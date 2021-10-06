89dc7788f73786a3fa8af4dbf2a69f3b0a7a083d
Antony Harfield <antonyharfield@gmail.com>
2021-10-06 11:52:49 +0100

    Add junit

---
+++ role-admin/src/test/java/org/rfcx/guardian/admin/comms/swm/SwmUtilsTest.kt
@@ -0,0 +1,12 @@
+package org.rfcx.guardian.admin.comms.swm
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class SwmUtilsTest {
+    @Test
+    fun canTestSwmUtils() {
+        assertEquals(1, 1)
+    }
+
+}
