f948e969886c03b9a0a7371382342e8fbb438af3
wasuphon <wasuphonn59@email.nu.ac.th>
2021-11-18 19:40:59 +0700

    rename swm network

--- role-admin/src/main/java/org/rfcx/guardian/admin/companion/CompanionPingJsonUtils.java
+++ role-admin/src/main/java/org/rfcx/guardian/admin/companion/CompanionPingJsonUtils.java
@@ -74,9 +74,9 @@ public class CompanionPingJsonUtils {
 			includeMeasuredAt = true;
 		}
 
-		if (includeAllExtraFields || ArrayUtils.doesStringArrayContainString(includeExtraFields, "swm_background")) {
+		if (includeAllExtraFields || ArrayUtils.doesStringArrayContainString(includeExtraFields, "swm_network")) {
 			String swmRssi = getConcatMetaField(app.swmMetaDb.dbSwmDiagnostic.getLatestRowAsJsonArray());
-			if (swmRssi.length() > 0) { jsonObj.put("swm_background", swmRssi); }
+			if (swmRssi.length() > 0) { jsonObj.put("swm_network", swmRssi); }
 			includeMeasuredAt = true;
 		}
 
