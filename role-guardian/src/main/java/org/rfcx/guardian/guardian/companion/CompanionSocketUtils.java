e9d9f2cc49231b1bc9bbe75757f6220d08246025
wasuphon <wasuphonn59@email.nu.ac.th>
2022-02-21 18:46:30 +0700

    Add timezone to ping

--- role-guardian/src/main/java/org/rfcx/guardian/guardian/companion/CompanionSocketUtils.java
+++ role-guardian/src/main/java/org/rfcx/guardian/guardian/companion/CompanionSocketUtils.java
@@ -14,6 +14,7 @@ import org.rfcx.guardian.utility.rfcx.RfcxLog;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.TimeZone;
 
 public class CompanionSocketUtils {
 
@@ -51,6 +52,8 @@ public class CompanionSocketUtils {
 			companionObj.put("checkin", getLatestAllSentCheckInType());
 
 			companionObj.put("local_time", System.currentTimeMillis());
+
+			companionObj.put("timezone", TimeZone.getDefault().getID());
 			
 		} catch (JSONException e) {
 			RfcxLog.logExc(logTag, e);
