d8856619bb6ae1b29d6b3a5b5e2921aebd582201
KritsanaMike <93254568+KritsanaMike@users.noreply.github.com>
2022-03-28 18:10:34 +0700

    fix code playsound
    - read filename in folder then random one name to play sound
    - move location sleep and change from 1 sec to 10 sec

--- playsound/play_sound.py
+++ playsound/play_sound.py
@@ -1,12 +1,22 @@
 from playsound import playsound
 import time
 import random
+import os 
+
+path = 'sound/'
+
+def readfile():
+    file = os.listdir(path)
+    return file
 
 def play_sound():
-    name = ['dog','truck','motorcycle','chainsaw']
-    ran = random.choice(name)
-    print("Playing..."+ran)
-    playsound(ran+'.wav')
+    name = readfile()
+    if len(name) > 0:
+        ran = random.choice(name)
+        print("Playing..."+ran)
+        playsound(path+ran)
+    else:
+        print("No file sound")
 
 while True:
     result = time.localtime()
@@ -15,12 +25,11 @@ while True:
     print(str(t_hr)+":"+str(t_min))
 
     if t_hr >= 9 and t_hr < 22:
-        if t_min == 00 or t_min == 30:
+        if t_min == 0 or t_min == 30:
             print(t_hr)
             play_sound()
-        time.sleep(1)
     elif  t_hr == 22:
-        if t_min == 00:
+        if t_min == 0:
             print(t_hr)
-            play_sound()
-        
+            play_sound()         
+    time.sleep(10)
