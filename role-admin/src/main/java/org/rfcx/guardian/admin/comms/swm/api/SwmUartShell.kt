467de7b2256e1ab1ac0d80e47657fa3d02903bfd
TooSeriuz <frongs@rfcx.org>
2022-10-25 16:07:34 +0700

    Revert "Add log"
    This reverts commit 4c0ce91f72862b766ef758b2b63b17e936e48c92.

--- role-admin/src/main/java/org/rfcx/guardian/admin/comms/swm/api/SwmUartShell.kt
+++ role-admin/src/main/java/org/rfcx/guardian/admin/comms/swm/api/SwmUartShell.kt
@@ -1,18 +1,11 @@
 package org.rfcx.guardian.admin.comms.swm.api
 
-import android.os.Environment
 import android.util.Log
 import org.rfcx.guardian.admin.RfcxGuardian
 import org.rfcx.guardian.utility.device.hardware.DeviceHardware_OrangePi_3G_IOT
-import org.rfcx.guardian.utility.misc.DateTimeUtils
 import org.rfcx.guardian.utility.misc.FileUtils
 import org.rfcx.guardian.utility.misc.ShellCommands
 import org.rfcx.guardian.utility.rfcx.RfcxLog
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileWriter
-import java.io.IOException
-import java.util.*
 
 class SwmUartShell(
     private val ttyPath: String = DeviceHardware_OrangePi_3G_IOT.DEVICE_TTY_FILEPATH_SATELLITE,
@@ -35,31 +28,11 @@ class SwmUartShell(
     override fun execute(request: String, timeout: Int): List<String> {
         val ttyCommand = makeTtyCommand(request, timeout)
         try {
-            commandToLog(request)
             return ShellCommands.executeCommandAsRoot(ttyCommand, false)
         } catch (e: Exception) {
             RfcxLog.logExc(logTag, e)
         }
         return listOf()
-    }
-
-    private fun commandToLog(command: String) {
-        val logFile = File(Environment.getExternalStorageDirectory().absolutePath + "/SwarmLog.txt")
-        if (!logFile.exists()) {
-            try {
-                logFile.createNewFile()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-        try {
-            val buf = BufferedWriter(FileWriter(logFile, true))
-            buf.append("${DateTimeUtils.getDateTime()}:$command")
-            buf.newLine()
-            buf.close()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
     }
 
     private fun makeTtyCommand(input: String, timeout: Int = 1): String {
